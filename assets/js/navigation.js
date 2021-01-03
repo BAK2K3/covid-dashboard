00e080dd91d0904108a70ec3c8dea0d5b794f683
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-01-03 12:03:16 +0000

    Add functionality for toggler to change colour of main covid-data section for testing.

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,9 +1,23 @@
 $(document).ready(function () {
 
+    // On button Click
     $(".btn").on("click", function () {
+        // Checks if the button is already active
         if (!$(this).hasClass("btn-active")) {
+            // If not, remove active class from all .btn classes, and add to this button.
             $(".btn").removeClass("btn-active");
             $(this).addClass("btn-active");
+
+            $(".covid-data-container").removeClass("background-test-1 background-test-2 background-test-3");
+
+            // Checks which button has been pressed, and adds respective background color for testing
+            if ($(this).hasClass("btn-compare")) {
+                $(".covid-data-container").addClass("background-test-1");
+            } else if ($(this).hasClass("btn-visualise")) {
+                $(".covid-data-container").addClass("background-test-2");
+            } else if ($(this).hasClass("btn-map")) {
+                $(".covid-data-container").addClass("background-test-3");
+            }
         }
     });
 });
