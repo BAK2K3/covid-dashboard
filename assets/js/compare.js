48efb96ff7ba2f5f2cda7a0087921daec153e58b
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-01-04 08:19:29 +0000

    Add second country parameter to JSON request, and reformat output HTML to table rows.

--- assets/js/compare.js
+++ assets/js/compare.js
@@ -1,33 +1,48 @@
 // Function for API request for specified country
 function searchFunction() {
 
-    // Obtain country selected from dropdown
-    var targetCountry = $("#countrySelect").val();
+    // Obtain countries selected from dropdown menus
+    var targetCountry1 = $("#countrySelect1").val();
+    var targetCountry2 = $("#countrySelect2").val();
 
-    // Query the REST API based on given target country
+    // Query the REST API based on given target countries
     $.when(
-        $.getJSON(`https://disease.sh/v3/covid-19/countries/${targetCountry}`)
+        $.getJSON(`https://disease.sh/v3/covid-19/countries/${targetCountry1}%2C%20${targetCountry2}`)
+
     ).then(
         function (response) {
+            console.log(response);
+
             // Obtain the list of keys for the response
-            var keys = Object.keys(response);
+            var keys = Object.keys(response[0]);
             var outputHTML = "";
 
-            // For each key, generate given value, and append to HTML
+            // For each key, generate given value for each country, and append to HTML
             keys.forEach(function (el) {
 
-                var jsonValue;
-                // Checks whether the key is "updated", and if so converts it do date format.
+                // declare the variables for each key 
+                var jsonValue1;
+                var jsonValue2;
+
+                // Checks whether the key is "updated", and if so converts the responses to date format.
                 if (el === "updated") {
-                    jsonValue = new Date(response[el]);
+                    jsonValue1 = new Date(response[0][el]);
+                    jsonValue2 = new Date(response[1][el]);
                 } else {
-                    jsonValue = response[el];
+                    jsonValue1 = response[0][el];
+                    jsonValue2 = response[1][el];
                 }
-                outputHTML += `<p><strong>${el}</strong>: ${jsonValue}</p>`;
+
+                // Add the variables for countries 1 and 2 to a table row, and add to existing html output
+                outputHTML += ` <tr>
+                                    <td>${jsonValue1}</td>
+                                    <td><strong>${el}</strong></td>
+                                    <td>${jsonValue2}</td>
+                                </tr >`
             })
 
             // Set Output field to "output HTML" as generated above.
-            $("#output-field").html(outputHTML);
+            $("#covid-table-body").html(outputHTML);
 
             // Catch errors
         }, function (errorResponse) {
@@ -49,8 +64,9 @@ function fetchCountries() {
                 return `<option value="${item.country}">${item.country}</option>`;
             })
 
-            // Insert the generated list into the Country Select Drop down
-            $("#countrySelect").html(listItemsHTML);
+            // Insert the generated list into the Country Select Drop downs
+            $("#countrySelect1").html(listItemsHTML);
+            $("#countrySelect2").html(listItemsHTML);
 
             // Catch errors
         }, function (errorResponse) {
