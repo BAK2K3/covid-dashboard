3c905a179c01160f53566a9488eca4024ee7274c
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-01-04 07:41:52 +0000

    Update API address to disease.sh, and add date formatting.

--- assets/js/compare.js
+++ assets/js/compare.js
@@ -6,7 +6,7 @@ function searchFunction() {
 
     // Query the REST API based on given target country
     $.when(
-        $.getJSON(`https://corona.lmao.ninja/v2/countries/${targetCountry}`)
+        $.getJSON(`https://disease.sh/v3/covid-19/countries/${targetCountry}`)
     ).then(
         function (response) {
             // Obtain the list of keys for the response
@@ -15,11 +15,18 @@ function searchFunction() {
 
             // For each key, generate given value, and append to HTML
             keys.forEach(function (el) {
-                outputHTML += `<p><strong>${el}</strong>: ${response[el]}</p>`;
+
+                var jsonValue;
+                // Checks whether the key is "updated", and if so converts it do date format.
+                if (el === "updated") {
+                    jsonValue = new Date(response[el]);
+                } else {
+                    jsonValue = response[el];
+                }
+                outputHTML += `<p><strong>${el}</strong>: ${jsonValue}</p>`;
             })
 
             // Set Output field to "output HTML" as generated above.
-
             $("#output-field").html(outputHTML);
 
             // Catch errors
@@ -34,7 +41,7 @@ function fetchCountries() {
 
     // Query API for name of all countries
     $.when(
-        $.getJSON("https://corona.lmao.ninja/v2/countries?sort=country")
+        $.getJSON("https://disease.sh/v3/covid-19/countries")
     ).then(
         // Generate list of all country names available, and create HTML for these as options in a drop down options
         function (response) {
