600ec5026b68dff1f9f148dd6e32df477bc3e4ca
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-01-13 08:21:55 +0000

    Add check to formatTimeSeries function to see whether current country is already selected, and add additional config parameters to graph.

--- assets/js/visualise.js
+++ assets/js/visualise.js
@@ -7,53 +7,85 @@ var graphConfig = {};
 // Function for formatting the required data
 function formatTimeSeries(update) {
 
-    // Reset dataset
-    graphConfig = {};
-
     // Find the index position of the country within the visual compare dataset
     let indexTargetCountry = globalVisualDataset.findIndex(function (entry) {
         return entry.country == $('#countrySelectVisualise').val();
     });
 
-    // Define the country, data labels, and extract the required data
+    // Extract chosen country from selector
     var countryLabel = globalVisualDataset[indexTargetCountry].country;
-    var timescaleLabels = Object.keys(globalVisualDataset[indexTargetCountry].timeline.cases);
-    var fullData = Object.entries(globalVisualDataset[indexTargetCountry].timeline);
 
-    let datasets = [];
-    // Iterate through the datasets contained (cases, recovered, deaths)
-    fullData.forEach(element => {
+    // Check whether current graph exists, and is also not already displaying country selected, otherwise skips whole next section 
+    if ((typeof (myLineChart) == "undefined") || (myLineChart.options.title.text !== countryLabel)) {
 
-        // Create a new object
-        let obj = {};
-        // Format the data appropriately for the maps script.
-        obj["data"] = Object.values(element[1]);
-        obj["label"] = element[0];
-        // Generate random colour for the line
-        obj["borderColor"] = '#' + Math.floor(Math.random() * 16777215).toString(16);
-        obj["fill"] = false;
-        // Push the current object into the dataset array
-        datasets.push(obj);
+        // Reset dataset
+        graphConfig = {};
 
-    });
+        // Define data labels, and extract the required data
+        var timescaleLabels = Object.keys(globalVisualDataset[indexTargetCountry].timeline.cases);
+        var fullData = Object.entries(globalVisualDataset[indexTargetCountry].timeline);
 
-    graphConfig = {
-        type: 'line',
-        data: {
-            labels: timescaleLabels,
-            datasets: datasets
+        let datasets = [];
+        // Iterate through the datasets contained (cases, recovered, deaths)
+        fullData.forEach(element => {
+
+            // Create a new object
+            let obj = {};
+            // Format the data appropriately for the maps script.
+            obj["data"] = Object.values(element[1]);
+            obj["label"] = element[0];
+            // Generate random colour for the line
+            obj["borderColor"] = '#' + Math.floor(Math.random() * 16777215).toString(16);
+            obj["fill"] = false;
+            // Push the current object into the dataset array
+            datasets.push(obj);
+
+        });
+
+        // Graph configuration
+        graphConfig = {
+            type: 'line',
+            data: {
+                labels: timescaleLabels,
+                datasets: datasets
+            },
+            options: {
+                responsive: true,
+                title: {
+                    display: true,
+                    text: countryLabel
+                },
+                scales: {
+                    xAxes: [{
+                        display: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Date'
+                        }
+                    }],
+                    yAxes: [{
+                        display: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Total'
+                        }
+                    }]
+                }
+            }
         }
-    }
 
-    if (update == false) {
-        // Call generate graph function with newly created country label, date label, and datasets
-        generateGraph();
-    } else {
-        // Update the graph with the newly requested data
-        myLineChart.data.labels = timescaleLabels;
-        myLineChart.data.datasets = datasets;
-        // Update the graph
-        myLineChart.update();
+        // Checks whether parameter "update" has been provided to parent function
+        if (update == false) {
+            // Call generate graph function with newly created country label, date label, and datasets
+            generateGraph();
+        } else {
+            // Replace the graph with the newly requested data
+            myLineChart.data.labels = timescaleLabels;
+            myLineChart.data.datasets = datasets;
+            myLineChart.options.title.text = countryLabel;
+            // Update the graph
+            myLineChart.update();
+        }
     }
 }
 
