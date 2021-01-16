21a7423e5742e72ab80578fe129744b8e09237e3
BAK2K3 <benjamin.a.kavanagh@gmail.com>
2021-01-16 19:53:39 +0000

    Remove requirement for button to be used to execute table, and adapt styling for <>= accordingly.

--- assets/js/compare.js
+++ assets/js/compare.js
@@ -5,69 +5,107 @@ function formatTableData() {
     var firstTargetCountry = $("#firstCountrySelect").val();
     var secondTargetCountry = $("#secondCountrySelect").val();
 
-    // Find the index position of each country within the global compare dataset
-    var firstTargetCountryIndex = globalCompareDataset.findIndex(function (entry) {
-        return entry.country == firstTargetCountry
-    });
-    var secondTargetCountryIndex = globalCompareDataset.findIndex(function (entry) {
-        return entry.country == secondTargetCountry
-    });
+    // If not default value
+    if (firstTargetCountry !== "none") {
+        // Find the index position of each country within the global compare dataset
+        var firstTargetCountryIndex = globalCompareDataset.findIndex(function (entry) {
+            return entry.country == firstTargetCountry
+        });
+    }
 
-    // Obtain the list of keys for the response (statistic names)
+    // If not default value
+    if (secondTargetCountry !== "none") {
+        var secondTargetCountryIndex = globalCompareDataset.findIndex(function (entry) {
+            return entry.country == secondTargetCountry
+        });
+    }
+
+    // Obtain the list of keys for the compare dataset (statistic names)
     var statisticsKey = Object.keys(globalCompareDataset[0]);
+    // Initiate HTML
     var outputHTML = "";
 
     // For each key, generate given value for each country, and append to HTML
     statisticsKey.forEach(function (el) {
 
-        // Checks the statistic is not "Null" in the dictionary
+        // Checks the statistic is not "Null" in the dictionary, otherwise skips
         if (statisticDictionary[el] !== null) {
 
             // declare the data variables for each country's statistic 
             var firstCountryData;
             var secondCountryData;
 
+            // Checks whether the target country is the default, and set the value accordingly
+            if (firstTargetCountry == "none") {
+                firstCountryData = "";
+            } else if (secondTargetCountry == "none") {
+                secondCountryData = "";
+            }
+
             // Checks whether the statistic is "updated", and if so converts the responses to shorthand date format.
             if (el === "updated") {
-                firstCountryData = new Date(globalCompareDataset[firstTargetCountryIndex][el]);
-                firstCountryData = firstCountryData.toDateString() + " " + firstCountryData.getHours() + ":" + firstCountryData.getMinutes();
-                secondCountryData = new Date(globalCompareDataset[secondTargetCountryIndex][el]);
-                secondCountryData = secondCountryData.toDateString() + " " + secondCountryData.getHours() + ":" + secondCountryData.getMinutes();
+
+                if (firstTargetCountry !== "none") {
+                    firstCountryData = new Date(globalCompareDataset[firstTargetCountryIndex][el]);
+                    firstCountryData = firstCountryData.toDateString() + " " + firstCountryData.getHours() + ":" + firstCountryData.getMinutes();
+                }
+                if (secondTargetCountry !== "none") {
+                    secondCountryData = new Date(globalCompareDataset[secondTargetCountryIndex][el]);
+                    secondCountryData = secondCountryData.toDateString() + " " + secondCountryData.getHours() + ":" + secondCountryData.getMinutes();
+                }
 
                 // Checks whether the statistic is "country", and if so returns raw string.
             } else if (el == "country") {
-                firstCountryData = globalCompareDataset[firstTargetCountryIndex][el];
-                secondCountryData = globalCompareDataset[secondTargetCountryIndex][el];
+                if (firstTargetCountry !== "none") {
+                    firstCountryData = globalCompareDataset[firstTargetCountryIndex][el];
+                }
+                if (secondTargetCountry !== "none") {
+                    secondCountryData = globalCompareDataset[secondTargetCountryIndex][el];
+                }
 
                 // Otherwise, returns a formatted number string (i.e comma seperated) along with respective formatting for bigger/smaller values
             } else {
 
-                // Define number variables for each country, for later calculation and referencing
-                let firstCountryNumber = globalCompareDataset[firstTargetCountryIndex][el];
-                let secondCountryNumber = globalCompareDataset[secondTargetCountryIndex][el];
+                let firstCountryNumber;
+                let secondCountryNumber;
 
-                // Assign the "Data" variables the string locale formatted number
-                // Locale Number Formatting https://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
-                firstCountryData = Number(firstCountryNumber).toLocaleString();
-                secondCountryData = Number(secondCountryNumber).toLocaleString();
+                // First Country
+                if (firstTargetCountry !== "none") {
+                    // Define number variables for each country, for later calculation and referencing
+                    firstCountryNumber = globalCompareDataset[firstTargetCountryIndex][el];
+                    // Assign the "Data" variables the string locale formatted number
+                    firstCountryData = Number(firstCountryNumber).toLocaleString();
+                }
 
-                // Determine which number is larger, and format styling accordingly.
-                if (firstCountryNumber > secondCountryNumber) {
+                // Second Country
+                if (secondTargetCountry !== "none") {
+                    // Define number variables for each country, for later calculation and referencing
+                    secondCountryNumber = globalCompareDataset[secondTargetCountryIndex][el];
+                    // Assign the "Data" variables the string locale formatted number
+                    secondCountryData = Number(secondCountryNumber).toLocaleString();
+                }
 
-                    // Style for if first Country number is larger
-                    firstCountryData = '<i class="fas fa-long-arrow-alt-up"></i> ' + firstCountryData;
-                    secondCountryData = secondCountryData + ' <i class="fas fa-long-arrow-alt-down"></i>'
+                // If neither of the values are default, add the symbols for <>=
+                if ((firstTargetCountry !== "none") || (secondTargetCountry !== "none")) {
 
-                } else if (firstCountryNumber < secondCountryNumber) {
+                    // Determine which number is larger, and format styling accordingly.
+                    if (firstCountryNumber > secondCountryNumber) {
 
-                    // Style for if second Country number is larger
-                    firstCountryData = '<i class="fas fa-long-arrow-alt-down"></i> ' + firstCountryData;
-                    secondCountryData = secondCountryData + ' <i class="fas fa-long-arrow-alt-up"></i>'
-                } else {
+                        // Style for if first Country number is larger
+                        firstCountryData = '<i class="fas fa-long-arrow-alt-up"></i> ' + firstCountryData;
+                        secondCountryData = secondCountryData + ' <i class="fas fa-long-arrow-alt-down"></i>'
 
-                    // Style for if numbers are the same
-                    firstCountryData = '<i class="fas fa-equals"></i> ' + firstCountryData;
-                    secondCountryData = secondCountryData + ' <i class="fas fa-equals"></i>';
+                    } else if (firstCountryNumber < secondCountryNumber) {
+
+                        // Style for if second Country number is larger
+                        firstCountryData = '<i class="fas fa-long-arrow-alt-down"></i> ' + firstCountryData;
+                        secondCountryData = secondCountryData + ' <i class="fas fa-long-arrow-alt-up"></i>'
+                    } else if (firstCountryNumber == secondCountryNumber) {
+
+                        // Style for if numbers are the same
+                        firstCountryData = '<i class="fas fa-equals"></i> ' + firstCountryData;
+                        secondCountryData = secondCountryData + ' <i class="fas fa-equals"></i>';
+                    }
                 }
             }
 
@@ -83,10 +121,12 @@ function formatTableData() {
 // On either country select changing
 $("#firstCountrySelect").add("#secondCountrySelect").on('change', function () {
 
-    // Check whether the initial table has been generated
-    if ($("#covidTableBody").html() != '<tr><td colspan="3"> Please select two countries to compare statistics.</td></tr>') {
-
-        // If so, re-perform the search function to update table.
+    // If both values are default
+    if ($("#firstCountrySelect").val() == "none" && $("#secondCountrySelect").val() == "none") {
+        // Reset the table
+        $("#covidTableBody").html(`<tr><td colspan ='3' > Please select two countries to compare statistics.</td></tr>`);
+    } else {
+        // Otherwise, perform search function to update table.
         formatTableData();
     }
 })
